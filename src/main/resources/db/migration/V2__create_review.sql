eee7f45731da0e758d9dbff0d3758b7c9cd4e19c
kimunche <angela_id@naver.com>
2023-09-09 03:24:59 +0900

    [ISSUE-10] flyway v2 추가

---
+++ src/main/resources/db/migration/V2__create_review.sql
@@ -0,0 +1,10 @@
+CREATE TABLE review(
+    seq bigint auto_increment,
+    star_rate int,
+    contents varchar(200),
+    user_id varchar(20) not null,
+    hotel_no bigint not null,
+    PRIMARY KEY (seq),
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (hotel_no) REFERENCES hotel(hotel_no)
+)
