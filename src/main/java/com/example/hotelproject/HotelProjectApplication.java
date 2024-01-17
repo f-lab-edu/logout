9aabf56761d98db703a3d6c062e98f64585d1b35
f-lab-moony <mooony@f-lab.kr>
2024-01-17 11:30:58 +0900

    인프라 구성

--- src/main/java/com/example/hotelproject/HotelProjectApplication.java
+++ src/main/java/com/example/hotelproject/HotelProjectApplication.java
@@ -1,12 +1,7 @@
 package com.example.hotelproject;
 
-import com.example.hotelproject.util.Animal;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -17,14 +12,5 @@ public class HotelProjectApplication {
         SpringApplication.run(HotelProjectApplication.class, args);
     }
 
-    @Bean
-    public ApplicationRunner applicationRunner(@Qualifier("dog") Animal animal) {
-        return new ApplicationRunner() {
-            @Override
-            public void run(ApplicationArguments args) throws Exception {
-                animal.bark();
-            }
-        };
-    }
 
 }
